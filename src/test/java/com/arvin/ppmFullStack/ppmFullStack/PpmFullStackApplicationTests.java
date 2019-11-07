a9a168616784d1802227b61453439d875cc74d3a
Arvin Azawi <arvinazawi@arvins-mbp.home>
2019-11-06 23:55:26 -0500

    update

---
+++ src/test/java/com/arvin/ppmFullStack/ppmFullStack/PpmFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvin.ppmFullStack.ppmFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
